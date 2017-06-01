e31e3f63c73e9013e6649013357ed8ca753827a0
EstebanMagallonPerez <esteban1995perez@gmail.com>
2017-05-31 21:49:03 -0700

    It works, It sends messages, and shows messages

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ IOS DEPENDANCIES/SETUP
 	- install SQLite 3.x from Cydia/Telesphoreo repo
 	- install CLSMS from https:// s1ris.github.io/repo
 */
-
-
-
-
 var fs = require('fs');
 
 var express = require('express');
@@ -18,166 +14,30 @@ var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 var persistantSocket;
 
-
-
-
-
-
-
-
-
-var SSH = require('simple-ssh');
-
-function echo(callback) {
-	var ssh = new SSH({
-		host: 'host',//Find your devices ip address, settings>wifi>tap connected network> IP Address
-		user: 'root',
-		pass: 'alpine'//Default is alpine, You SHOULD change it
-	});
-
-	var data = '';
-	var error = null;
-
-	ssh.exec("sqlite3 -header ../mobile/Library/SMS/sms.db <<EOF\n"+
-			 ".mode insert\n"+/*
-			 ".separator '‰‰'\n"+*/
-			 "Select message.ROWID, message.handle_id, text, chat_identifier from message, chat, chat_handle_join, chat_message_join where chat.ROWID = chat_handle_join.chat_id AND chat_handle_join.handle_id = message.handle_id AND chat.ROWID = chat_message_join.chat_id AND message_id = message.ROWID GROUP BY chat_identifier ORDER BY date DESC LIMIT 5;\n"+
-			 "EOF\n", {
-		out: function(stdout) {
-			data += stdout;
-		},
-		exit: function(code) {
-			if (code != 0) return callback(new Error('exit code: ' + code));
-
-			// here's all the data you have persisted from stdout
-			return callback(error, data);
-		},
-		error: function(err) {
-			error = err;
-		}
-	}).start();
-}
-
-
-function sendSide()
-{
-	echo(function(err, data) {
-		if (err) {
-			// handle potential err
-			console.error(err.stack);
-		} else {
-			console.log("before: ");
-			console.log(data);
-			var output = [];
-			data = data.replace(/INSERT INTO table VALUES/g, "‰‰");
-			var tempSplit = data.split(/‰‰/);
-			tempSplit.shift(1);
-			for(var i = 0; i < tempSplit.length; i += 4)
-			{
-				output.push({"ROWID":tempSplit[i],"handle_id":tempSplit[i+1],"text":tempSplit[i+2],"chat_identifier":tempSplit[i+3]});
-			}
-			console.log("after: ")
-			console.log(data);
-			console.log(output);
-			persistantSocket.send({content:"side",messages:output});
-		}
-	});
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-"sqlite3 -header sms.db <<EOF
-	.mode csv
-		.separator '‰‰'
-Select message.ROWID, message.handle_id, text, chat_identifier from message, chat, chat_handle_join, chat_message_join where chat.ROWID = chat_handle_join.chat_id AND chat_handle_join.handle_id = message.handle_id AND chat.ROWID = chat_message_join.chat_id AND message_id = message.ROWID GROUP BY chat_identifier ORDER BY date DESC;
-EOF
-"
-*/
-
-
-//get into sqlite3 terminal
-/*function getSide(socket)
-{
-	console.log("calling get side");
-
-	ssh.exec("sqlite3 -header ../mobile/Library/SMS/sms.db <<EOF\n"+
-			 ".mode list\n"+
-			 ".separator '‰‰'\n"+
-			 "Select message.ROWID, message.handle_id, text, chat_identifier from message, chat, chat_handle_join, chat_message_join where chat.ROWID = chat_handle_join.chat_id AND chat_handle_join.handle_id = message.handle_id AND chat.ROWID = chat_message_join.chat_id AND message_id = message.ROWID GROUP BY chat_identifier ORDER BY date DESC;\n"+
-			 "EOF\n", {
-		out: function(stdout) {
-			console.log("we are going to send stuff");
-			var output = [];
-			stdout = stdout.replace(/\n/g, "‰‰");
-			var tempSplit = stdout.split(/‰‰/);
-			for(var i = 0; i < tempSplit.length; i += 4)
-			{
-				output.push({"ROWID":tempSplit[i],"handle_id":tempSplit[i+1],"text":tempSplit[i+2],"chat_identifier":tempSplit[i+3]});
-			}
-			console.log(stdout);
-			socket.send({content:"side",messages:output});
-			//console.log(stdout.split(/\r?\n/));
-		},
-		err: function(stderr) {
-			console.log(stderr); // this-does-not-exist: command not found
-		}
-	}).start();
-}*/
-
-
-/*
-‰ <-column separator
-½ <-row separator
-
-*/
-
-
-/*ssh
-	.exec('cd ../mobile/Library/SMS\n ls', {
-	out: function(stdout) {
-		console.log(stdout);
-	}
-}).start();*/
-/*ssh.exec('ls', {
-	out: function(stdout) {
-		console.log(stdout);
-	}
-}).start();*/
-
-
-
-
 app.use(express.static(__dirname + '/html'));
 
 app.get('/', function(req, res){
 	res.sendfile('./html/page.html');
 });
-//Whenever someone connects this gets executed
+
 io.on('connection', function(socket){
-	//start(socket);
 	persistantSocket = socket;
 	sendSide();
-	//Whenever someone disconnects this piece of code executed
-	socket.on('clientEvent', function(data){
-		//	getMessages(socket,data);
+
+	socket.on('requestMessages', function(data){
+		sendMessagesToClient(data);
+	});
+	socket.on('sendMessage', function(data){
+		var obj = (fs.readFileSync('./config.json', 'utf8'));
+		obj = JSON.parse(obj);
+		var test = {
+			host: obj.host,//Find your devices ip address, settings>wifi>tap connected network> IP Address
+			user: obj.user,
+			pass: obj.pass//Default is alpine, You SHOULD change it
+		}
+		var ssh = new SSH(test);
+		ssh.exec('clsms "'+data.content+'" '+data.recipient).start();
+		console.log(data);
 	});
 
 });
@@ -186,88 +46,139 @@ server.listen(3000, function(){
 	console.log('nodeJS Messages is now online');
 });
 
+var SSH = require('simple-ssh');
 
+function getSide(callback) {
+	var obj = (fs.readFileSync('./config.json', 'utf8'));
+	obj = JSON.parse(obj);
+	var test = {
+		host: obj.host,//Find your devices ip address, settings>wifi>tap connected network> IP Address
+		user: obj.user,
+		pass: obj.pass//Default is alpine, You SHOULD change it
+	}
+	var ssh = new SSH(test);
 
+	var data = '';
+	var error = null;
 
+	ssh.exec("sqlite3 -header ../mobile/Library/SMS/sms.db <<EOF\n"+
+			 ".mode insert\n"+/*
+			 ".separator '‰‰'\n"+*/
+			 "Select message.ROWID, message.handle_id, chat_identifier,text from message, chat, chat_handle_join, chat_message_join where chat.ROWID = chat_handle_join.chat_id AND chat_handle_join.handle_id = message.handle_id AND chat.ROWID = chat_message_join.chat_id AND message_id = message.ROWID GROUP BY chat_identifier ORDER BY date DESC;\n"+
+			 "EOF\n", {
+		out: function(stdout) {
+			data += stdout;
+		},
+		exit: function(code) {
+			if (code != 0) return callback(new Error('exit code: ' + code));
+			return callback(error, data);
+		},
+		error: function(err) {
+			error = err;
+		}
+	}).start();
+}
 
-
-
-
-
-
-
-/*
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function downloadMessages()
+function sendSide()
 {
-	sftp.connect({
-		host: '192.168.1.112',
-		port: '22',
-		username: 'root',
-		password: 'alpine'
-	}).then(() => {
-		return ;
-	}).then((data) => {
-		const remoteFilename = '../mobile/Library/SMS/sms.db';
-		const localFilename = './messages/messages.db';
-		sftp.get(remoteFilename,null,null).then((stream) => {
-			stream.on('end', function () {
-				db = new sqlite3.Database('./messages/messages.db');
-				start();
-				sftp.end();
-			});
-			stream.pipe(fs.createWriteStream(localFilename));
-		});
-		return;
-	}).catch((err) => {
-		console.log(err, 'catch error');
+	getSide(function(err, data) {
+		if (err) {
+			console.error(err.stack);
+		} else {
+			var output = [];
+			console.log(data);
+			data = data.replace(/INSERT INTO table VALUES/g, "‰‰");
+			var currentIndex = 0;
+			var cutLocation;
+			while(data.length>1)
+			{
+				let message = {"ROWID":null,"handle_id":null,"chat_identifier":null,"text":null}
+				cutLocation = data.indexOf(",");
+				message.ROWID = data.substring(3, cutLocation);
+				data = data.substr(cutLocation+1)
+
+				cutLocation = data.indexOf(",");
+				message.handle_id = data.substring(0, cutLocation);
+				data = data.substr(cutLocation+1)
+
+				cutLocation = data.indexOf(",");
+				var chatFormat = data.indexOf("'");
+				if( chatFormat < cutLocation)
+				{
+					message.chat_identifier = data.substring(1, cutLocation-1);
+				}else
+				{
+					message.chat_identifier = data.substring(0, cutLocation);
+				}
+				data = data.substr(cutLocation+2)
+
+				cutLocation = data.indexOf("');\n");
+				message.text = data.substring(0, cutLocation);
+				output.push(message);
+				data = data.substr(cutLocation+4)
+			}
+			persistantSocket.send({content:"side",messages:output});
+		}
 	});
 }
 
-if(0)
-{
-	downloadMessages();
-}else
-{
-	db = new sqlite3.Database('./messages/messages.db');
-}
-function getMessages(socket,handleID)
-{
-	var output = [];
+function getMessages(chat_identifier,callback) {
+	var obj = (fs.readFileSync('./config.json', 'utf8'));
+	obj = JSON.parse(obj);
+	var test = {
+		host: obj.host,//Find your devices ip address, settings>wifi>tap connected network> IP Address
+		user: obj.user,
+		pass: obj.pass//Default is alpine, You SHOULD change it
+	}
+	var ssh = new SSH(test);
 
-	db.all("Select text,is_from_me from message, chat, chat_handle_join, chat_message_join where chat.ROWID = chat_handle_join.chat_id AND chat_handle_join.handle_id = message.handle_id AND chat.ROWID = chat_message_join.chat_id AND message_id = message.ROWID AND message.handle_id = "+handleID+" ORDER BY date ASC",function(err,rows){
-		for(let i =0;i < rows.length;i++)
-		{
-			output.push(rows[i])
+	var data = '';
+	var error = null;
+	ssh.exec("sqlite3 -header ../mobile/Library/SMS/sms.db <<EOF\n"+
+			 ".mode insert\n"+/*
+			 ".separator '‰‰'\n"+*/
+			 "Select is_from_me,service,text from message, chat, chat_handle_join, chat_message_join where chat.ROWID = chat_handle_join.chat_id AND chat_handle_join.handle_id = message.handle_id AND chat.ROWID = chat_message_join.chat_id AND message_id = message.ROWID AND chat_identifier = '"+chat_identifier+"' ORDER BY date ASC;\n"+
+			 "EOF\n", {
+		out: function(stdout) {
+			data += stdout;
+		},
+		exit: function(code) {
+			if (code != 0) return callback(new Error('exit code: ' + code));
+			return callback(error, data);
+		},
+		error: function(err) {
+			error = err;
 		}
-		socket.send({content:"main",messages:output});
-	})
+	}).start();
 }
-function start(socket)
-{
-	//Get all chats with different people
-	var output = [];
 
-	db.all("Select message.ROWID, message.handle_id, text, chat_identifier from message, chat, chat_handle_join, chat_message_join where chat.ROWID = chat_handle_join.chat_id AND chat_handle_join.handle_id = message.handle_id AND chat.ROWID = chat_message_join.chat_id AND message_id = message.ROWID GROUP BY chat_identifier ORDER BY date DESC;",function(err,rows){
-		for(let i =0;i < rows.length;i++)
-		{
-			output.push(rows[i])
+function sendMessagesToClient(chat_identifier)
+{
+	getMessages(chat_identifier,function(err, data) {
+		if (err) {
+			console.error(err.stack);
+		} else {
+			var output = [];
+			data = data.replace(/INSERT INTO table VALUES/g, "‰‰");
+			var currentIndex = 0;
+			var cutLocation;
+			console.log(data);
+			while(data.length>1)
+			{
+				let message = {"is_from_me":null,"service":null,"text":null}
+				cutLocation = data.indexOf(",");
+				message.is_from_me = parseInt(data.substring(3, cutLocation));
+				data = data.substr(cutLocation+1)
+				cutLocation = data.indexOf(",");
+				message.service = data.substring(1, cutLocation-1);
+				data = data.substr(cutLocation+2)
+
+				cutLocation = data.indexOf("');\n");
+				message.text = data.substring(0, cutLocation);
+				output.push(message);
+				data = data.substr(cutLocation+4)
+			}
+			persistantSocket.send({content:"main",messages:output});
 		}
-		socket.send({content:"side",messages:output});
-	})
-}*/
+	});
+}
